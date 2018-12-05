dfcc2af307a93fa164f9ca55d3c4ba47c669f9cf
SeduireplutotqueVioler <acaron217@etud.univ-tln.fr>
2018-12-05 17:11:23 +0100

    script shell

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
-CMD_DM restart default
-eval "$(CMD_DM env --no-proxy default)"
+docker-machine --storage-path /scratch/$USER restart default
+eval "$(docker-machine --storage-path /scratch/$USER env --no-proxy default)"
 cd dockerpayaracompose
 docker-compose build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$http_proxy payara-full
 docker-compose up -d payara-full
