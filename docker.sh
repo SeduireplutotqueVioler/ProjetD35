6ed2899c3c84d763c89a446da2ac25c849a71bb9
SeduireplutotqueVioler <acaron217@etud.univ-tln.fr>
2018-12-05 17:04:44 +0100

    docker

--- docker.sh
+++ docker.sh
@@ -1,2 +1,6 @@
 CMD_DM restart default
 eval "$(CMD_DM env --no-proxy default)"
+cd dockerpayaracompose
+docker-compose build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$http_proxy payara-full
+docker-compose up -d payara-full
+cd ..
